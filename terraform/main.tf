d8d667ef433d5ffd7e200657dd08f5ef574c701d
nickwokeupinseoul <153803820+nickwokeupinseoul@users.noreply.github.com>
2024-02-01 23:18:06 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,5 +72,22 @@ resource "yandex_compute_instance" "prod" {
 
 }
 
+data "yandex_compute_image" "ubuntu_image" {
+  family = "ubuntu-2004-lts"
+}
 
+# boot disk for vm-assembly = ubuntu 20.04 with 15GB
+resource "yandex_compute_disk" "serv-sbor_ubuntu2004_15GB" {
+  type     = "network-ssd"
+  zone     = "ru-central1-a"
+  image_id = data.yandex_compute_image.ubuntu_image.id
+  size = 15
+}
+# boot disk for vm-prod = ubuntu 20.04 with 15GB
+resource "yandex_compute_disk" "serv-prod_ubuntu2004_15GB" {
+  type     = "network-ssd"
+  zone     = "ru-central1-a"
+  image_id = data.yandex_compute_image.ubuntu_image.id
+  size = 15
+}
 
